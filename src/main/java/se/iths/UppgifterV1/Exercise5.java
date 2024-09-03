a8e6e74b574a4003b26b1755894d2aef4131da38
ehottinen <ehottinen@gmail.com>
2024-09-03 10:15:44 +0200

    Initial commit

--- src/main/java/se/iths/UppgifterV1/Exercise5.java
+++ src/main/java/se/iths/UppgifterV1/Exercise5.java
@@ -6,7 +6,8 @@ public class Exercise5 {
 
     //odo: skriv ett program som låter användaren ange 3 heltal som argument
     //todo: om färre än 3 tal eller fler än 3 tal anges ska ett felmeddelande skrivas ut och programmet avslutas
-    //todo: om annat än siffror 0-9 anges ska programmet ge ett felmeddelande men fortsätta köra programmet och ersätta argumentet som har andra tecken än 0-9 med 0
+    //todo: om annat än siffror 0-9 anges ska programmet ge ett felmeddelande men fortsätta köra programmet
+    // och ersätta argumentet som har andra tecken än 0-9 med 0
     //odo: När 3 giltiga tal finns ska det räkna ut det största och skriva ut talet
 
     public static void main(String[] args) {
---
+++ src/main/java/se/iths/UppgifterV1/Exercise8.java
@@ -0,0 +1,17 @@
+package se.iths.UppgifterV1;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+
+        //todo: skapa en array med jämnt antal tal men minst 4 ex: [1,2,1,3,3,4]
+        //todo: jämför de första två talen med de nästa två talen
+        //todo: programmet ska då ta reda på om någon eller båda siffrorna i det första paret förekommer i det andra paret
+        //todo: om det är sant fortsätt jämförelsen men flytta nu fram två steg
+        //todo: om alla par i arrayen har minst en siffra gemensamt ska programmet skriva ut Everywhere
+        //todo: men om någon jämförelse mellan två par saknar likadana siffror ska programmet skriva ut 50t Somewhere
+        //todo: och om inga par delar siffror ska programmet skriva ut Nowhere
+
+        int[] myNum = {1,2,3,3,4};
+
+    }
+}
