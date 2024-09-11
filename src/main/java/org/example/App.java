bd2a8ed7f703c24997450a8885a52c0f1ed2be1d
unhel <ehottinen@gmail.com>
2024-09-11 22:54:58 +0200

    Skrivit ut meny och skapat funktioner utan funktioner =)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,62 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner sc = new Scanner(System.in);
+
+        //PrintMenu();
+        //String name = sc.nextLine();
+
+        while(true){
+            Meny();
+            String name = sc.nextLine();
+            switch(name){
+                case "1":
+                    Inmatning(sc);
+                    break;
+                case "2":
+                    MinMaxMedel(sc);
+                    break;
+                case "3":
+                    Sortera(sc);
+                    break;
+                case "4":
+                    BästaLaddningstid(sc);
+                    break;
+            }
+            if (name.equals("e") || name.equals("E")){
+                break;
+            }
+        }
+
+    }
+    public static void Meny(){
+        System.out.println("Elpriser");
+        System.out.println("========");
+        System.out.println("1. Inmatning");
+        System.out.println("2. Min, Max och Medel");
+        System.out.println("3. Sortera");
+        System.out.println("4. Bästa Laddningstid (4h)");
+        System.out.println("e. Avsluta");
+    }
+    public static void Inmatning(Scanner sc){
+        System.out.println("Inmatning");
+        System.out.println("Uuuuuuuugh!?!");
+        sc.nextLine();
+    }
+    public static void MinMaxMedel(Scanner sc){
+        System.out.println("Min, Max och Medel");
+        sc.nextLine();
+    }
+    public static void Sortera(Scanner sc){
+        System.out.println("Sortera");
+        sc.nextLine();
+    }
+    public static void BästaLaddningstid(Scanner sc){
+        System.out.println("Bästa Laddningstid (4h)");
+        sc.nextLine();
     }
 }
